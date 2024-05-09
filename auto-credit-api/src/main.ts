af2e5ef8896d720b06209d948c15094ea5504dd2
artphil <artphil.dev@gmail.com>
2024-05-09 10:27:03 -0300

    build: setup swagger

--- auto-credit-api/src/main.ts
+++ auto-credit-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auto Credit API')
+    .setDescription('API for the Auto Credit System')
+    .setVersion('0.0.1')
+    .addTag('credit')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
