e3ca6e0fd08feef24e531b288ed7d98acfde6ede
artphil <artphil.dev@gmail.com>
2024-05-13 16:53:04 -0300

    build: setup validations

--- auto-credit-api/src/main.ts
+++ auto-credit-api/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Auto Credit API')
