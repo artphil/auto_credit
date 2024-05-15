ef1055e8dc90a6f9a8713673b8e60708f2a7903c
artphil <artphil.dev@gmail.com>
2024-05-15 10:56:08 -0300

    build: setup typeorm

--- auto-credit-api/src/app/app.module.ts
+++ auto-credit-api/src/app/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PostgresConfigService } from 'src/config/postgres.config.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
