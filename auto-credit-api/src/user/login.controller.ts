fc401798069e61e8100457af09083053f969ce43
artphil <artphil.dev@gmail.com>
2024-05-19 16:38:36 -0300

    feat: enable cors

--- auto-credit-api/src/user/login.controller.ts
+++ auto-credit-api/src/user/login.controller.ts
@@ -1,13 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { UserLoginDTO } from './dto/userLogin.dto';
+import { UserResponseDTO } from './dto/userResponse.dto';
 
 @ApiTags('Login')
 @Controller('login')
 export class LoginController {
   constructor(private service: UserService) {}
 
+  @ApiResponse({
+    status: 200,
+    type: UserResponseDTO,
+  })
   @Post()
   async create(@Body() user: UserLoginDTO) {
     return await this.service.login(user.username, user.password);
